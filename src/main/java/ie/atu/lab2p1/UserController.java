53bbe6dcf94940f554ba6ba52b87cc0f9cf43230
G00398070 <G00398070@atu.ie>
2023-10-05 14:15:38 +0100

    added registerUserBody to userController

--- src/main/java/ie/atu/lab2p1/UserController.java
+++ src/main/java/ie/atu/lab2p1/UserController.java
@@ -4,6 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.sql.SQLOutput;
 
 @Controller
 public class UserController {
@@ -16,5 +20,11 @@ public class UserController {
     @GetMapping("registerUser/{name}/{email}")
     public void registerUser(@PathVariable String name, @PathVariable String email){
 
+    }
+
+    @PostMapping("registerUserBody")
+    public void registerUserBody(@RequestBody User user){
+        System.out.println(user.name);
+        System.out.println(user.email);
     }
 }
