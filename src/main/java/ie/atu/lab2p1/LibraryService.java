33a405ef1a62684496b16d5ead54c66941420cbf
G00398070 <G00398070@atu.ie>
2023-10-05 15:45:47 +0100

    created libraryService class with addBook and getBooks methods

---
+++ src/main/java/ie/atu/lab2p1/LibraryService.java
@@ -0,0 +1,15 @@
+package ie.atu.lab2p1;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LibraryService {
+
+    private void addBook(Book book){
+        //code to add book to repo
+    }
+
+    private void getBooks() {
+        //code to return list of all books
+    }
+}
