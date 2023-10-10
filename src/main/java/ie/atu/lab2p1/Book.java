8f2e941e10682aec2617e5d0ad51db1b436645fe
G00398070 <G00398070@atu.ie>
2023-10-10 20:34:40 +0100

    changed code in book class

--- src/main/java/ie/atu/lab2p1/Book.java
+++ src/main/java/ie/atu/lab2p1/Book.java
@@ -1,47 +1,19 @@
 package ie.atu.lab2p1;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.util.Date;
+
 
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class Book {
-    private String title;
-    private String author;
-    private int isbn;
-    private int publishedYear;
+    @Getter @Setter private String title;
+    @Getter @Setter private String author;
+    @Getter @Setter private int isbn;
+    @Getter @Setter private int publishedYear;
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public int getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(int isbn) {
-        this.isbn = isbn;
-    }
-
-    public int getPublishedYear() {
-        return publishedYear;
-    }
-
-    public void setPublishedYear(int publishedYear) {
-        this.publishedYear = publishedYear;
-    }
 }
